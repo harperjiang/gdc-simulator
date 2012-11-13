2c31ed0b4a91c8f965722d0e7a7b3fecc7ac630e
llw0417@gmail.com <llw0417@gmail.com@30a95390-2462-41a1-4906-c7b42bd86c71>
2012-11-13 02:54:24 +0000

    Add Framework for time based simulation

---
+++ GDCSimulator/src/edu/clarkson/gdc/simulator/framework/Pipe.java
@@ -0,0 +1,45 @@
+package edu.clarkson.gdc.simulator.framework;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Queue;
+import java.util.concurrent.ConcurrentLinkedQueue;
+
+import edu.clarkson.gdc.simulator.framework.DataEvent.PathNode;
+
+public class Pipe extends Component {
+
+	private Queue<DataEvent> buffer;
+
+	public Pipe() {
+		super();
+		this.buffer = new ConcurrentLinkedQueue<DataEvent>();
+	}
+
+	public List<DataEvent> get() {
+		List<DataEvent> output = new ArrayList<DataEvent>();
+		while (true) {
+			DataEvent event = this.buffer.peek();
+			if (event.getTimestamp() >= getClock().getCounter())
+				output.add(this.buffer.poll());
+			else {
+				break;
+			}
+		}
+		return output;
+	}
+
+	public void put(DataEvent event) {
+		event.access(new PathNode(this, getClock().getCounter() + getLatency()));
+		buffer.offer(event);
+	}
+
+	@Override
+	public void send() {
+	}
+	
+	@Override
+	public void process() {
+		
+	}
+}

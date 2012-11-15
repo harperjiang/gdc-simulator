910bcfee6f17473e973c5c66cdfcdd7d3c3a5712
harperjiang@gmail.com <harperjiang@gmail.com@30a95390-2462-41a1-4906-c7b42bd86c71>
2012-11-15 18:06:27 +0000

    Add some testcases

---
+++ trunk/GDCSimulator/test/edu/clarkson/gdc/simulator/framework/ClockTest.java
@@ -0,0 +1,14 @@
+package edu.clarkson.gdc.simulator.framework;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ClockTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ trunk/GDCSimulator/test/edu/clarkson/gdc/simulator/framework/NodeTest.java
@@ -0,0 +1,33 @@
+package edu.clarkson.gdc.simulator.framework;
+
+import static org.junit.Assert.*;
+
+import java.util.List;
+import java.util.Map;
+
+import org.junit.Test;
+
+public class NodeTest {
+
+	@Test
+	public void testProcess() {
+		
+	}
+
+	@Test
+	public void testSend() {
+		Node node = new Node() {
+			@Override
+			protected ProcessResult process(Map<Pipe, List<DataEvent>> events) {
+				return null;
+			}
+		};
+		
+		Pipe pipe = new Pipe();
+		pipe.setId("1-2");
+		node.getInputs().add(pipe);
+		
+		
+	}
+
+}
